68b81ca3583d96e4340e9893252a04024f0b4038
mogit2020 <mohibullah.kamal@gmail.com>
2018-11-04 19:57:31 +0100

    Add unctionality to Cockpit.js

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,4 +1,29 @@
-const cockpit = () => {
-    
+import React from 'react';
+
+const cockpit = (props) => {
+
+    // let classes = ['red', 'bold'].join(' ');   //Very smart... the following array joins the two text into one "red bold"; now we can add to className in JSX
+    const assignedClasses = [];
+    if (this.props.persons.length <= 2) {
+        assignedClasses.push(classes.red);   //classes = ['red']
+    }
+    if (this.props.persons.length <= 1) {
+        assignedClasses.push(classes.bold);   //classes = ['red', 'bold']
+    }
+
+    return (
+        <div>
+            <h1>Hi, I am a React App</h1>
+            <p className={assignedClasses.join(' ')}>This is really working!!</p>
+
+            <button
+            className= {btnClass}
+            // style={style}   //removing it because we no longer using inline props
+            onClick = {this.togglePersonsHandler}>
+                Toggle Persons
+            </button>
+        </div>
+    );
 };
 
+export default cockpit;
--- src/containers/App.js
+++ src/containers/App.js
@@ -80,27 +80,8 @@ class App extends Component {
       // }   //remove hovering code because we are removing "radium" library
     }
 
-    // let classes = ['red', 'bold'].join(' ');   //Very smart... the following array joins the two text into one "red bold"; now we can add to className in JSX
-    const assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red);   //classes = ['red']
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold);   //classes = ['red', 'bold']
-    }
-    
-
-
     return (
       <div className={classes.App}>
-        <h1>Hi, I am a React App</h1>
-        <p className={assignedClasses.join(' ')}>This is really working!!</p>
-
-        {/* onClick is JSX syntax... */}
-        <button
-          className= {btnClass}
-          // style={style}   //removing it because we no longer using inline props
-          onClick = {this.togglePersonsHandler}> Toggle Persons</button>
         {persons}
       </div>
     );
