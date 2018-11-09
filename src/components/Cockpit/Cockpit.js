eeea2c5f194b93c936a921b0a4289372205081ce
Mohibullah Kamal <mohibullah.kamal@gmail.com>
2018-11-09 21:18:12 +0000

    Update Cockpit.js

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -6,6 +6,12 @@ const cockpit = (props) => {
 
     // let classes = ['red', 'bold'].join(' ');   //Very smart... the following array joins the two text into one "red bold"; now we can add to className in JSX
     const assignedClasses = [];
+    let btnClass = '';
+    
+    if (props.showPersons) {
+        btnClass = classes.Red;
+    }
+    
     if (this.props.persons.length <= 2) {
         assignedClasses.push(classes.red);   //classes = ['red']
     }
@@ -14,7 +20,7 @@ const cockpit = (props) => {
     }
 
     return (
-        <div>
+        <div className={classes.Cockpit}>
             <h1>Hi, I am a React App</h1>
             <p className={assignedClasses.join(' ')}>This is really working!!</p>
 
